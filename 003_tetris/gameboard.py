3ba120b67443f5a1a90a37d97961158a5cf2405d
anreith <patrikfirek@gmail.com>
2016-07-06 20:00:01 +0200

    area now has builtin offset

--- 003_tetris/gameboard.py
+++ 003_tetris/gameboard.py
@@ -1,60 +1,76 @@
 from itertools import chain
 
 class Area:
-  def __init__(self, w, h):
+  def __init__(self, w, h, ox=0, oy=0, fill=0):
     assert w > 0 and h > 0, "Width, height must be positive integers"
 
     self.w = w
     self.h = 0
+    self.ox = ox
+    self.oy = oy
     self.lines = []
-    [self.addLine(y=0, val=0) for _ in range(0,h)]
+    [self.addLine(y=0, fill=fill) for _ in range(0,h)]
 
   def getDimension(self):
+    '''Return our dimensions'''
     return self.w, self.h
 
+  def getOffset(self):
+    '''Return our offset'''
+    return self.ox, self.oy
+
   def getLine(self, y):
-    '''return iterator to line'''
+    '''return line value iterator'''
     assert not self.isPointOutside(0,y), "{} out of bounds".format(y)
     return iter(self.lines[y])
 
   def remLine(self, y):
+    '''remove line'''
     assert not self.isPointOutside(0,y), "{} out of bounds".format(y)
     del self.lines[y]
     self.h -= 1
 
-  def addLine(self, y, val):
-    '''Add empty line at y'''
+  def addLine(self, y, fill=0):
+    '''Add empty line at y optionally filled'''
     assert y >= 0 and y <= self.h
-    self.lines.insert(y, [val] * self.w)
+    self.lines.insert(y, [fill] * self.w)
     self.h += 1
 
   def setVal(self, x, y, val):
+    '''Set value at given coordinates'''
     assert not self.isPointOutside(x,y), "{},{} out of bounds".format(x,y)
     self.lines[y][x] = val
 
   def getVal(self, x, y):
+    '''Get value at given coordinates'''
     assert not self.isPointOutside(x,y), "{},{} out of bounds".format(x,y)
     return self.lines[y][x]
 
-  def getLinePoints(self, y, dx=0, dy=0):
-    '''Return points on a line that are non-empty with optional translation'''
-    return ((x+dx,y+dy, v) for x,v in enumerate(self.lines[y]) if v)
+  def getLinePoints(self, y):
+    '''Return points on area line including offset'''
+    return ((x+self.ox,y+self.oy, v) for x,v in enumerate(self.lines[y]) if v)
 
-  def getPoints(self, dx=0, dy=0):
+  def getPoints(self):
     '''Return points with value and optional translation'''
-    return chain(*(self.getLinePoints(y, dx, dy) for y in range(0,self.h)))
+    return chain(*(self.getLinePoints(y) for y in range(0,self.h)))
 
   def isPointOutside(self, x,y):
-    return x < 0 or x >= self.w or y < 0 or y >= self.h
+    '''True if point is outside including offset'''
+    rx = x - self.ox
+    ry = y - self.oy
+    return rx < 0 or rx >= self.w or ry < 0 or ry >= self.h
 
-  def getOutsidePoints(self, other, dx=0, dy=0):
-    return ((x,y,v) for x,y,v in other.getPoints(dx, dy) if self.isPointOutside(x,y))
+  def getOutsidePoints(self, other):
+    '''Get points from other area that are outside this area'''
+    return ((x,y,v) for x,y,v in other.getPoints() if self.isPointOutside(x,y))
 
-  def getInsidePoints(self, other, dx=0, dy=0):
-    return ((x,y,v) for x,y,v in other.getPoints(dx, dy) if not self.isPointInside(x,y))
+  def getInsidePoints(self, other):
+    '''Get points from other are that are inside this area'''
+    return ((x,y,v) for x,y,v in other.getPoints() if not self.isPointOutside(x,y))
 
-  def isOverLappedBy(self, other, dx, dy):
-    return any(self.getVal(x,y) for x,y,_ in getInsideBoundsPoints(self, other, dx, dy))
+  def isOverLappedBy(self, other):
+    '''True if other area points overlap points in this area'''
+    return any(self.getVal(x,y) for x,y,_ in self.getInsidePoints(other))
 
   def __str__(self):
     strs = []
